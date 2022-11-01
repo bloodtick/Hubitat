4c5ac45209c3138cd6e945d2d05fb06f5a9cb6fc
Bloodtick Jones <12273404+bloodtick@users.noreply.github.com>
2022-11-01 08:43:20 -0400

    Update replicaEnvisalink.groovy

--- hubiThingsReplica/devices/replicaEnvisalink.groovy
+++ hubiThingsReplica/devices/replicaEnvisalink.groovy
@@ -15,7 +15,7 @@ metadata
 {
     definition(name: "Replica Envisalink", namespace: "hubitat", author: "bloodtick", importUrl:"https://raw.githubusercontent.com/bloodtick/Hubitat/main/hubiThingsReplica/devices/replicaEnvisalink.groovy",)
 	{
-	capability "Actuator"
+        capability "Actuator"
         capability "Alarm"
         capability "Refresh"
         
