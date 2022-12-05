b5c9c5a8b90e201b496e4be80d4ff0713eca596f
Bloodtick Jones <12273404+bloodtick@users.noreply.github.com>
2022-12-05 17:22:35 -0500

    1.2.0

--- hubiThingsReplica/devices/replicaButton.groovy
+++ hubiThingsReplica/devices/replicaButton.groovy
@@ -12,7 +12,7 @@
 *
 */
 @SuppressWarnings('unused')
-public static String version() {return "1.1.2"}
+public static String version() {return "1.2.0"}
 
 metadata 
 {
@@ -20,24 +20,17 @@ metadata
     {
         capability "Actuator"
         capability "Battery"
+        capability "Configuration"
         capability "DoubleTapableButton"
         capability "HoldableButton"
         capability "PushableButton"
+        capability "Refresh"
         capability "ReleasableButton"
         capability "TemperatureMeasurement"
-        
-        attribute "command", "enum", ["doubleTap.*", "hold.*", "push.*", "release.*"]
+
         attribute "healthStatus", "enum", ["offline", "online"]
-        
-        command "setBatteryValue", [[name: "battery*", type: "NUMBER", description: "Battery level in %"]]
-        command "setDoubleTappedValue", [[name: "doubleTapped*", type: "NUMBER", description: "Button number double tapped"]]  
-        command "setHeldValue", [[name: "held*", type: "NUMBER", description: "Button number held"]]
-        command "setNumberOfButtonsValue", [[name: "numberOfButtons*", type: "NUMBER", description: "Number of device buttons"]]
-        command "setPushedValue", [[name: "pushed*", type: "NUMBER", description: "Button number pushed"]]
-        command "setReleasedValue", [[name: "released*", type: "NUMBER", description: "Button number released"]]
-        command "setTemperatureValue", [[name: "temperature*", type: "NUMBER", description: "Temperature level in F or C"]]
-        command "setHealthStatusValue", [[name: "healthStatus*", type: "ENUM", description: "Any supported healthStatus attribute", constraints: ["offline","online"]]]
-        command "replicaRules" // indicates getReplicaRules is available
+    }
+    preferences {   
     }
 }
 
@@ -46,26 +39,44 @@ def installed() {
 }
 
 def updated() {
-	initialize()
+	initialize()    
 }
 
 def initialize() {
+    updateDataValue("triggers", groovy.json.JsonOutput.toJson(getReplicaTriggers()))
+    updateDataValue("commands", groovy.json.JsonOutput.toJson(getReplicaCommands()))
 }
 
-def parse(String description) {
-    log.info "${device.displayName} parse"
+def configure() {
+    log.info "${device.displayName} configured default rules"
+    initialize()
+    updateDataValue("rules", getReplicaRules())
+    sendCommand("configure")
+}
+
+// Methods documented here will show up in the Replica Command Configuration. These should be mostly setter in nature. 
+Map getReplicaCommands() {
+    return ([ "setBatteryValue":[[name:"battery*",type:"NUMBER"]], "setDoubleTappedValue":[[name:"buttonNumber",type:"NUMBER"]], "setHeldValue":[[name:"buttonNumber",type:"NUMBER"]], "setNumberOfButtonsValue":[[name:"numberOfButtons*",type:"NUMBER"]],             
+              "setPushedValue":[[name:"buttonNumber",type:"NUMBER"]], "setReleasedValue":[[name:"buttonNumber",type:"NUMBER"]], "setTemperatureValue":[[name:"temperature*",type:"NUMBER"]], "setHealthStatusValue":[[name:"healthStatus*",type:"ENUM"]]
+            ])
 }
 
 def setBatteryValue(value) {
-    sendEvent(name: "battery", value: value, unit: "%", descriptionText: "${device.displayName} battery level is $value %")
+    String descriptionText = "${device.displayName} battery level is $value %"
+    sendEvent(name: "battery", value: value, unit: "%", descriptionText: descriptionText)
+    log.info descriptionText
 }
 
 def setDoubleTappedValue(value=1) {
-    sendEvent(name: "doubleTapped", value: value, descriptionText: "${device.displayName} button $value was double tapped", isStateChange: true)
+    String descriptionText = "${device.displayName} button $value was double tapped"
+    sendEvent(name: "doubleTapped", value: value, descriptionText: descriptionText, isStateChange: true)
+    log.info descriptionText
 }
 
 def setHeldValue(value=1) {
-    sendEvent(name: "held", value: value, descriptionText: "${device.displayName} button $value was held", isStateChange: true)
+    String descriptionText = "${device.displayName} button $value was held"
+    sendEvent(name: "held", value: value, descriptionText: descriptionText, isStateChange: true)
+    log.info descriptionText
 }
 
 def setNumberOfButtonsValue(value=1) {
@@ -73,46 +84,57 @@ def setNumberOfButtonsValue(value=1) {
 }
 
 def setPushedValue(value=1) {
-    sendEvent(name: "pushed", value: value, descriptionText: "${device.displayName} button $value was pushed", isStateChange: true)
+    String descriptionText = "${device.displayName} button $value was pushed"
+    sendEvent(name: "pushed", value: value, descriptionText: descriptionText, isStateChange: true)
+    log.info descriptionText
 }
 
 def setReleasedValue(value=1) {
-    sendEvent(name: "released", value: value, descriptionText: "${device.displayName} button $value was released", isStateChange: true)
+    String descriptionText = "${device.displayName} button $value was released"
+    sendEvent(name: "released", value: value, descriptionText: descriptionText, isStateChange: true)
+    log.info descriptionText
 }
 
 def setTemperatureValue(value) {
     String unit = "°${getTemperatureScale()}"
-    sendEvent(name: "temperature", value: value, unit: unit, descriptionText: "${device.displayName} temperature is $value $unit")
+    String descriptionText = "${device.displayName} temperature is $value $unit"
+    sendEvent(name: "temperature", value: value, unit: unit, descriptionText: descriptionText)
+    log.info descriptionText
 }
 
 def setHealthStatusValue(value) {    
     sendEvent(name: "healthStatus", value: value, descriptionText: "${device.displayName} healthStatus set to $value")
 }
 
-private def sendCommand(String value, Map data=null, Boolean isStateChange=true) {
-    sendEvent(name: "command", value: value, descriptionText: "${device.displayName} sending command ${data ? data : value }", data: data, isStateChange: isStateChange, displayed: false)
+// Methods documented here will show up in the Replica Trigger Configuration. These should be all of the native capability commands
+Map getReplicaTriggers() {
+    return ([ "doubleTap":[[name:"buttonNumber",type:"NUMBER"]], "hold":[[name:"buttonNumber",type:"NUMBER"]], "push":[[name:"buttonNumber",type:"NUMBER"]], "release":[[name:"buttonNumber",type:"NUMBER"]], "refresh":[]])
+}
+
+private def sendCommand(String name, def value=null, String unit=null, data=[:]) {
+    parent?.deviceTriggerHandler(device, [name:name, value:value, unit:unit, data:data, now:now])
 }
 
 def doubleTap(value=1) {
-    sendCommand("doubleTap.*", ["doubleTap.*":[doubleTap:value]])    
+    sendCommand("doubleTap", value)    
 }
 
 def hold(value=1) {
-    sendCommand("hold.*", ["hold.*":[hold:value]])    
+    sendCommand("hold", value)    
 }
 
 def push(value=1) {
-    sendCommand("push.*", ["push.*":[push:value]])    
+    sendCommand("push", value)    
 }
 
 def release(value=1) {
-    sendCommand("release.*", ["release.*":[release:value]])    
+    sendCommand("release", value)    
 }
 
-void replicaRules() {
-    log.info getReplicaRules()
+void refresh() {
+    sendCommand("refresh")
 }
 
 String getReplicaRules() {
-    return """{"components":[{"trigger":{"attribute: battery.*":{"title":"IntegerPercent","properties":{"value":{"type":"integer","minimum":0,"maximum":100},"unit":{"type":"string","enum":["%"],"default":"%"}},"additionalProperties":false,"required":["value"],"capability":"battery","attribute":"battery","label":"attribute: battery.*"}},"command":{"command: setBatteryValue(battery*)":{"arguments":["NUMBER"],"parameters":[{"name":"battery*","description":"Battery level in %","type":"NUMBER"}],"name":"setBatteryValue","label":"command: setBatteryValue(battery*)"}},"type":"smartTrigger","mute":true},{"trigger":{"attribute: button.double":{"properties":{"value":{"title":"ButtonState","type":"string","enum":["pushed","held","double","pushed_2x","pushed_3x","pushed_4x","pushed_5x","pushed_6x","down","down_2x","down_3x","down_4x","down_5x","down_6x","down_hold","up","up_2x","up_3x","up_4x","up_5x","up_6x","up_hold","swipe_up","swipe_down","swipe_left","swipe_right"]}},"additionalProperties":false,"required":["value"],"capability":"button","attribute":"button","label":"attribute: button.double","value":"double"}},"command":{"command: setDoubleTappedValue(doubleTapped*)":{"arguments":["NUMBER"],"parameters":[{"name":"doubleTapped*","description":"Button number double tapped","type":"NUMBER"}],"name":"setDoubleTappedValue","label":"command: setDoubleTappedValue(doubleTapped*)"}},"type":"smartTrigger","disableStatus":true},{"trigger":{"attribute: button.held":{"properties":{"value":{"title":"ButtonState","type":"string","enum":["pushed","held","double","pushed_2x","pushed_3x","pushed_4x","pushed_5x","pushed_6x","down","down_2x","down_3x","down_4x","down_5x","down_6x","down_hold","up","up_2x","up_3x","up_4x","up_5x","up_6x","up_hold","swipe_up","swipe_down","swipe_left","swipe_right"]}},"additionalProperties":false,"required":["value"],"capability":"button","attribute":"button","label":"attribute: button.held","value":"held"}},"command":{"command: setHeldValue(held*)":{"arguments":["NUMBER"],"parameters":[{"name":"held*","description":"Button number held","type":"NUMBER"}],"name":"setHeldValue","label":"command: setHeldValue(held*)"}},"type":"smartTrigger","disableStatus":true},{"trigger":{"attribute: button.pushed":{"properties":{"value":{"title":"ButtonState","type":"string","enum":["pushed","held","double","pushed_2x","pushed_3x","pushed_4x","pushed_5x","pushed_6x","down","down_2x","down_3x","down_4x","down_5x","down_6x","down_hold","up","up_2x","up_3x","up_4x","up_5x","up_6x","up_hold","swipe_up","swipe_down","swipe_left","swipe_right"]}},"additionalProperties":false,"required":["value"],"capability":"button","attribute":"button","label":"attribute: button.pushed","value":"pushed"}},"command":{"command: setPushedValue(pushed*)":{"arguments":["NUMBER"],"parameters":[{"name":"pushed*","description":"Button number pushed","type":"NUMBER"}],"name":"setPushedValue","label":"command: setPushedValue(pushed*)"}},"type":"smartTrigger","disableStatus":true},{"trigger":{"attribute: healthStatus.*":{"schema":{"properties":{"value":{"title":"HealthState","type":"string","enum":["offline","online"]}},"additionalProperties":false,"required":["value"]},"enumCommands":[],"capability":"healthCheck","attribute":"healthStatus","label":"attribute: healthStatus.* "}},"command":{"command: setHealthStatusValue(healthStatus*)":{"arguments":["ENUM"],"parameters":[{"name":"healthStatus*","description":"Any supported healthStatus attribute","type":"ENUM","constraints":["offline","online"]}],"name":"setHealthStatusValue","label":"command: setHealthStatusValue(healthStatus*)"}},"type":"smartTrigger","mute":true},{"trigger":{"attribute: temperature.*":{"properties":{"value":{"title":"TemperatureValue","type":"number","minimum":-460,"maximum":10000},"unit":{"type":"string","enum":["F","C"]}},"additionalProperties":false,"required":["value","unit"],"capability":"temperatureMeasurement","attribute":"temperature","label":"attribute: temperature.*"}},"command":{"command: setTemperatureValue(temperature*)":{"arguments":["NUMBER"],"parameters":[{"name":"temperature*","description":"Temperature level in F or C","type":"NUMBER"}],"name":"setTemperatureValue","label":"command: setTemperatureValue(temperature*)"}},"type":"smartTrigger","mute":true},{"trigger":{"attribute: numberOfButtons.*":{"properties":{"value":{"title":"PositiveInteger","type":"integer","minimum":0}},"additionalProperties":false,"required":["value"],"capability":"button","attribute":"numberOfButtons","label":"attribute: numberOfButtons.*"}},"command":{"command: setNumberOfButtonsValue(numberOfButtons*)":{"arguments":["NUMBER"],"parameters":[{"name":"numberOfButtons*","description":"Number of device buttons","type":"NUMBER"}],"name":"setNumberOfButtonsValue","label":"command: setNumberOfButtonsValue(numberOfButtons*)"}},"type":"smartTrigger","mute":true}]}"""
+    return """{"version":1,"components":[{"trigger":{"type":"attribute","properties":{"value":{"title":"PositiveInteger","type":"integer","minimum":0}},"additionalProperties":false,"required":["value"],"capability":"button","attribute":"numberOfButtons","label":"attribute: numberOfButtons.*"},"command":{"name":"setNumberOfButtonsValue","label":"command: setNumberOfButtonsValue(numberOfButtons*)","type":"command","parameters":[{"name":"numberOfButtons*","type":"NUMBER"}]},"type":"smartTrigger","mute":true},{"trigger":{"title":"IntegerPercent","type":"attribute","properties":{"value":{"type":"integer","minimum":0,"maximum":100},"unit":{"type":"string","enum":["%"],"default":"%"}},"additionalProperties":false,"required":["value"],"capability":"battery","attribute":"battery","label":"attribute: battery.*"},"command":{"name":"setBatteryValue","label":"command: setBatteryValue(battery*)","type":"command","parameters":[{"name":"battery*","type":"NUMBER"}]},"type":"smartTrigger","mute":true},{"trigger":{"type":"attribute","properties":{"value":{"title":"ButtonState","type":"string"}},"additionalProperties":false,"required":["value"],"capability":"button","attribute":"button","label":"attribute: button.held","value":"held","dataType":"ENUM"},"command":{"name":"setHeldValue","label":"command: setHeldValue(buttonNumber)","type":"command","parameters":[{"name":"buttonNumber","type":"NUMBER"}]},"type":"smartTrigger","disableStatus":true},{"trigger":{"type":"attribute","properties":{"value":{"title":"ButtonState","type":"string"}},"additionalProperties":false,"required":["value"],"capability":"button","attribute":"button","label":"attribute: button.double","value":"double","dataType":"ENUM"},"command":{"name":"setDoubleTappedValue","label":"command: setDoubleTappedValue(buttonNumber)","type":"command","parameters":[{"name":"buttonNumber","type":"NUMBER"}]},"type":"smartTrigger","disableStatus":true},{"trigger":{"type":"attribute","properties":{"value":{"title":"ButtonState","type":"string"}},"additionalProperties":false,"required":["value"],"capability":"button","attribute":"button","label":"attribute: button.pushed","value":"pushed","dataType":"ENUM"},"command":{"name":"setPushedValue","label":"command: setPushedValue(buttonNumber)","type":"command","parameters":[{"name":"buttonNumber","type":"NUMBER"}]},"type":"smartTrigger","disableStatus":true},{"trigger":{"type":"attribute","properties":{"value":{"title":"TemperatureValue","type":"number","minimum":-460,"maximum":10000},"unit":{"type":"string","enum":["F","C"]}},"additionalProperties":false,"required":["value","unit"],"capability":"temperatureMeasurement","attribute":"temperature","label":"attribute: temperature.*"},"command":{"name":"setTemperatureValue","label":"command: setTemperatureValue(temperature*)","type":"command","parameters":[{"name":"temperature*","type":"NUMBER"}]},"type":"smartTrigger","mute":true}]}"""
 }
