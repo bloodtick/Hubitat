3c5ffe89335bf8028b0f91bb498566c914283199
Bloodtick Jones <12273404+bloodtick@users.noreply.github.com>
2022-12-05 13:21:55 -0500

    1.2.0

--- hubiThingsReplica/devices/replicaWindowShade.groovy
+++ hubiThingsReplica/devices/replicaWindowShade.groovy
@@ -10,9 +10,9 @@
 *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
 *  for the specific language governing permissions and limitations under the License.
 *
-*  version 1.2.0
 */
-import groovy.json.*
+@SuppressWarnings('unused')
+public static String version() {return "1.2.0"}
     
 metadata 
 {
