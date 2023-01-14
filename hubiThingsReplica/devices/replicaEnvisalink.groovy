de3b3353378f9ff36f545239fa79cc86795e82d1
Bloodtick Jones <12273404+bloodtick@users.noreply.github.com>
2023-01-14 08:27:56 -0500

    1.3.0

--- hubiThingsReplica/devices/replicaEnvisalink.groovy
+++ hubiThingsReplica/devices/replicaEnvisalink.groovy
@@ -1,5 +1,5 @@
 /**
-*  Copyright 2022 Bloodtick
+*  Copyright 2023 Bloodtick
 *
 *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 *  in compliance with the License. You may obtain a copy of the License at:
@@ -12,7 +12,7 @@
 *
 */
 @SuppressWarnings('unused')
-public static String version() {return "1.2.0"} 
+public static String version() {return "1.3.0"} 
 
 metadata 
 {
@@ -42,7 +42,8 @@ metadata
         command "setSelection", [[name: "selection*", type: "STRING", description: "Set selection value"]]
         command "setSwitch", [[name: "switch*", type: "ENUM", description: "Set switch value", constraints: ["off","on"]]]
     }
-    preferences {   
+    preferences {
+        input(name:"deviceInfoDisable", type: "bool", title: "Disable Info logging:", defaultValue: false)
     }
 }
 
@@ -60,7 +61,7 @@ def initialize() {
 }
 
 def configure() {
-    log.info "${device.displayName} configured default rules"
+    logInfo "${device.displayName} configured default rules"
     initialize()
     updateDataValue("rules", getReplicaRules())
     sendCommand("configure")
@@ -78,7 +79,7 @@ Map getReplicaCommands() {
 def setAlarmValue(String value) {
     String descriptionText = "${device.displayName} alarm is $value"
     sendEvent(name: "alarm", value: value, descriptionText: descriptionText)
-    log.info descriptionText
+    logInfo descriptionText
 }
 
 def setAlarmBoth() {
@@ -100,25 +101,25 @@ def setAlarmStrobe() {
 def setLedStatusValue(value) {
     String descriptionText = "${device.displayName} led status is $value"
     sendEvent(name: "ledStatus", value: value, descriptionText: descriptionText)
-    log.info descriptionText
+    logInfo descriptionText
 }
 
 def setPartStatusValue(value) {
     String descriptionText = "${device.displayName} part status is $value"
     sendEvent(name: "partStatus", value: value, descriptionText: descriptionText)
-    log.info descriptionText
+    logInfo descriptionText
 }
 
 def setPartitionCommandValue(value) {
     String descriptionText = "${device.displayName} partition command is $value"
     sendEvent(name: "partitionCommand", value: value, descriptionText: descriptionText)
-    log.info descriptionText
+    logInfo descriptionText
 }
 
 def setSecuritySystemStatusValue(String value) {
     String descriptionText = "${device.displayName} security system status is $value"
     sendEvent(name: "securitySystemStatus", value: value, descriptionText: descriptionText)
-    log.info descriptionText
+    logInfo descriptionText
 }
 
 def setSecuritySystemStatusArmedAway() {
@@ -136,13 +137,13 @@ def setSecuritySystemStatusDisarmed() {
 def setSelectionValue(value) {
     String descriptionText = "${device.displayName} selection is $value"
     sendEvent(name: "selection", value: "$value", descriptionText: descriptionText)
-    log.info descriptionText
+    logInfo descriptionText
 }
 
 def setSwitchValue(value) {
     String descriptionText = "${device.displayName} switch was turned $value"
     sendEvent(name: "switch", value: value, descriptionText: descriptionText)
-    log.info descriptionText
+    logInfo descriptionText
 }
 
 def setSwitchOff() {
@@ -165,7 +166,8 @@ Map getReplicaTriggers() {
 }
 
 private def sendCommand(String name, def value=null, String unit=null, data=[:]) {
-    parent?.deviceTriggerHandler(device, [name:name, value:value, unit:unit, data:data, now:now])
+    data.version=version()
+    parent?.deviceTriggerHandler(device, [name:name, value:value, unit:unit, data:data, now:now()])
 }
 
 def armAway(value) {
@@ -228,3 +230,9 @@ String getReplicaRules() {
     return """{"version":1,"components":[{"trigger":{"name":"strobe","label":"command: strobe()","type":"command"},"command":{"name":"strobe","type":"command","capability":"alarm","label":"command: strobe()"},"type":"hubitatTrigger"},{"trigger":{"name":"siren","label":"command: siren()","type":"command"},"command":{"name":"siren","type":"command","capability":"alarm","label":"command: siren()"},"type":"hubitatTrigger"},{"trigger":{"name":"setLedStatus","label":"command: setLedStatus(ledStatus*)","type":"command","parameters":[{"name":"ledStatus*","type":"STRING"}]},"command":{"name":"setledStatus","arguments":[{"name":"value","optional":false,"schema":{"type":"string","maxLength":30}}],"type":"command","capability":"partyvoice23922.ledStatus","label":"command: setledStatus(value*)"},"type":"hubitatTrigger"},{"trigger":{"name":"setSelection","label":"command: setSelection(selection*)","type":"command","parameters":[{"name":"selection*","type":"STRING"}]},"command":{"name":"setSelection","arguments":[{"name":"value","optional":false,"schema":{"type":"string","maxLength":20}}],"type":"command","capability":"partyvoice23922.dscselectswitch2","label":"command: setSelection(value*)"},"type":"hubitatTrigger"},{"trigger":{"name":"setPartitionCommand","label":"command: setPartitionCommand(partitionCommand*)","type":"command","parameters":[{"name":"partitionCommand*","type":"STRING"}]},"command":{"name":"setPartitionCommand","arguments":[{"name":"value","optional":false,"schema":{"type":"string","maxLength":16}}],"type":"command","capability":"partyvoice23922.partitioncommand2","label":"command: setPartitionCommand(value*)"},"type":"hubitatTrigger"},{"trigger":{"name":"setPartStatus","label":"command: setPartStatus(partStatus*)","type":"command","parameters":[{"name":"partStatus*","type":"STRING"}]},"command":{"name":"setPartStatus","arguments":[{"name":"value","optional":false,"schema":{"type":"string","maxLength":16}}],"type":"command","capability":"partyvoice23922.partitionstatus2","label":"command: setPartStatus(value*)"},"type":"hubitatTrigger"},{"trigger":{"name":"off","label":"command: off()","type":"command"},"command":{"name":"off","type":"command","capability":"switch","label":"command: off()"},"type":"hubitatTrigger"},{"trigger":{"name":"disarm","label":"command: disarm()","type":"command"},"command":{"name":"disarm","type":"command","capability":"securitySystem","label":"command: disarm()"},"type":"hubitatTrigger"},{"trigger":{"name":"both","label":"command: both()","type":"command"},"command":{"name":"both","type":"command","capability":"alarm","label":"command: both()"},"type":"hubitatTrigger"},{"trigger":{"name":"armAway","label":"command: armAway(bypassAll*)","type":"command","parameters":[{"name":"bypassAll*","type":"BOOLEAN"}]},"command":{"name":"armAway","arguments":[{"name":"bypassAll","optional":false,"schema":{"type":"boolean"}}],"type":"command","capability":"securitySystem","label":"command: armAway(bypassAll*)"},"type":"hubitatTrigger"},{"trigger":{"name":"armStay","label":"command: armStay(bypassAll*)","type":"command","parameters":[{"name":"bypassAll*","type":"BOOLEAN"}]},"command":{"name":"armStay","arguments":[{"name":"bypassAll","optional":false,"schema":{"type":"boolean"}}],"type":"command","capability":"securitySystem","label":"command: armStay(bypassAll*)"},"type":"hubitatTrigger"},{"trigger":{"type":"attribute","properties":{"value":{"title":"SwitchState","type":"string"}},"additionalProperties":false,"required":["value"],"capability":"switch","attribute":"switch","label":"attribute: switch.*"},"command":{"name":"setSwitchValue","label":"command: setSwitchValue(switch*)","type":"command","parameters":[{"name":"switch*","type":"ENUM"}]},"type":"smartTrigger"},{"trigger":{"type":"attribute","properties":{"value":{"type":"string","maxLength":20}},"additionalProperties":false,"required":["value"],"capability":"partyvoice23922.dscselectswitch2","attribute":"selection","label":"attribute: selection.*"},"command":{"name":"setSelectionValue","label":"command: setSelectionValue(selection*)","type":"command","parameters":[{"name":"selection*","type":"STRING"}]},"type":"smartTrigger"},{"trigger":{"type":"attribute","properties":{"value":{"type":"string"}},"additionalProperties":false,"required":["value"],"capability":"securitySystem","attribute":"securitySystemStatus","label":"attribute: securitySystemStatus.*"},"command":{"name":"setSecuritySystemStatusValue","label":"command: setSecuritySystemStatusValue(securitySystemStatus*)","type":"command","parameters":[{"name":"securitySystemStatus*","type":"ENUM"}]},"type":"smartTrigger"},{"trigger":{"type":"attribute","properties":{"value":{"type":"string","maxLength":16}},"additionalProperties":false,"required":["value"],"capability":"partyvoice23922.partitioncommand2","attribute":"partitionCommand","label":"attribute: partitionCommand.*"},"command":{"name":"setPartitionCommandValue","label":"command: setPartitionCommandValue(partitionCommand*)","type":"command","parameters":[{"name":"partitionCommand*","type":"STRING"}]},"type":"smartTrigger"},{"trigger":{"type":"attribute","properties":{"value":{"type":"string","maxLength":16}},"additionalProperties":false,"required":["value"],"capability":"partyvoice23922.partitionstatus2","attribute":"partStatus","label":"attribute: partStatus.*"},"command":{"name":"setPartStatusValue","label":"command: setPartStatusValue(partStatus*)","type":"command","parameters":[{"name":"partStatus*","type":"STRING"}]},"type":"smartTrigger"},{"trigger":{"type":"attribute","properties":{"value":{"type":"string","maxLength":30}},"additionalProperties":false,"required":["value"],"capability":"partyvoice23922.ledStatus","attribute":"ledStatus","label":"attribute: ledStatus.*"},"command":{"name":"setLedStatusValue","label":"command: setLedStatusValue(ledStatus*)","type":"command","parameters":[{"name":"ledStatus*","type":"STRING"}]},"type":"smartTrigger"},{"trigger":{"type":"attribute","properties":{"value":{"title":"HealthState","type":"string"}},"additionalProperties":false,"required":["value"],"capability":"healthCheck","attribute":"healthStatus","label":"attribute: healthStatus.*"},"command":{"name":"setHealthStatusValue","label":"command: setHealthStatusValue(healthStatus*)","type":"command","parameters":[{"name":"healthStatus*","type":"ENUM"}]},"type":"smartTrigger","mute":true},{"trigger":{"name":"setSwitch","label":"command: setSwitch(switch*)","type":"command","parameters":[{"name":"switch*","type":"ENUM"}]},"command":{"name":"setSwitch","arguments":[{"name":"value","optional":false,"schema":{"type":"string"}}],"type":"command","capability":"partyvoice23922.dscstayswitch","label":"command: setSwitch(value*)"},"type":"hubitatTrigger"},{"trigger":{"type":"attribute","properties":{"value":{"title":"String","type":"string","maxLength":255}},"additionalProperties":false,"required":["value"],"capability":"securitySystem","attribute":"alarm","label":"attribute: alarm.*"},"command":{"name":"setAlarmValue","label":"command: setAlarmValue(alarm*)","type":"command","parameters":[{"name":"alarm*","type":"ENUM"}]},"type":"smartTrigger"}]}
 status: {"components":{"main":{"actuator":{},"switchLevel":{"level":{"value":73,"unit":"%","timestamp":"2022-11-11T23:16:54.163Z"}},"sensor":{},"switch":{"switch":{"value":"off","timestamp":"2022-12-06T02:41:37.500Z"}}}}}"""
 }
+
+private logInfo(msg)  { if(settings?.deviceInfoDisable != true) { log.info  "${msg}" } }
+private logDebug(msg) { if(settings?.deviceDebugEnable == true) { log.debug "${msg}" } }
+private logTrace(msg) { if(settings?.deviceTraceEnable == true) { log.trace "${msg}" } }
+private logWarn(msg)  { log.warn   "${msg}" }
+private logError(msg) { log.error  "${msg}" }
