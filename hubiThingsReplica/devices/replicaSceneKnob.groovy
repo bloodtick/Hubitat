81515659a071fcb8c1382aa33f5551d58a46d15b
Bloodtick Jones <12273404+bloodtick@users.noreply.github.com>
2023-02-16 12:14:12 -0500

    1.3.0

---
+++ hubiThingsReplica/devices/replicaSceneKnob.groovy
@@ -0,0 +1,156 @@
+/**
+*  Copyright 2023 Bloodtick
+*
+*  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License. You may obtain a copy of the License at:
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+*  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+*  for the specific language governing permissions and limitations under the License.
+*
+*/
+@SuppressWarnings('unused')
+public static String version() {return "1.3.0"}
+
+metadata 
+{
+    definition(name: "Replica Scene Knob", namespace: "replica", author: "bloodtick", importUrl:"https://raw.githubusercontent.com/bloodtick/Hubitat/main/hubiThingsReplica/devices/replicaSceneKnob.groovy")
+    {
+        capability "Actuator"
+        capability "Configuration"
+        capability "Switch"
+        capability "Refresh"
+        
+        attribute "healthStatus", "enum", ["offline", "online"]
+    }
+    preferences {
+        input(name:"deviceInfoDisable", type: "bool", title: "Disable Info logging:", defaultValue: false)
+        input(name:"deviceDebugEnable", type: "bool", title: "Enable Debug logging:", defaultValue: false) 
+    }
+}
+
+def installed() {
+	initialize()
+}
+
+def updated() {
+	initialize()    
+}
+
+def initialize() {
+    updateDataValue("triggers", groovy.json.JsonOutput.toJson(getReplicaTriggers()))
+    updateDataValue("commands", groovy.json.JsonOutput.toJson(getReplicaCommands()))
+}
+
+def configure() {
+    logInfo "${device.displayName} configured default rules"
+    initialize()
+    updateDataValue("rules", getReplicaRules())
+    sendCommand("configure")
+}
+
+// Methods documented here will show up in the Replica Command Configuration. These should be mostly setter in nature. 
+Map getReplicaCommands() {
+    return ([ "setSceneIdValue":[[name:"sceneId*",type:"STRING"]], "setSwitchOff":[], "setSwitchOn":[], "setHealthStatusValue":[[name:"healthStatus*",type:"ENUM"]]])
+}
+
+def setSceneIdValue(value) {
+    state.sceneId = value
+    refresh()
+}
+
+def setSwitchValue(value) {
+    String descriptionText = "${device.displayName} was turned $value"
+    sendEvent(name: "switch", value: value, descriptionText: descriptionText)
+    logInfo descriptionText
+}
+
+def setSwitchOff() {
+    setSwitchValue("off")
+}
+
+def setSwitchOn() {
+    unschedule('watchdog')
+    setSwitchValue("on")
+    runIn(2,off)
+}
+
+def watchdog() {
+    logWarn "${device.displayName} did not receive a timely response from SmartThings. Please ensure the corresponding SmartThings Virtual Switch is included in the scene actions and it updates to 'Turn on' when the scene is triggered."
+    setSwitchOn()
+    runIn(2,setSwitchOff)
+}
+
+def setHealthStatusValue(value) {    
+    sendEvent(name: "healthStatus", value: value, descriptionText: "${device.displayName} healthStatus set to $value")
+}
+
+// Methods documented here will show up in the Replica Trigger Configuration. These should be all of the native capability commands
+Map getReplicaTriggers() {
+    return ([ "off":[] , "on":[], "refresh":[]])
+}
+
+private def sendCommand(String name, def value=null, String unit=null, data=[:]) {
+    data.version=version()
+    parent?.deviceTriggerHandler(device, [name:name, value:value, unit:unit, data:data, now:now()])
+}
+
+def off() { 
+    sendCommand("off")
+}
+
+def on() {
+    if( triggerScene(state?.sceneId)?.statusCode == iHttpSuccess )
+        runIn(5, watchdog)
+}
+
+void refresh() {    
+    off()
+}
+
+String getReplicaRules() {
+    return """{"version":1,"components":[{"trigger":{"name":"off","label":"command: off()","type":"command"},"command":{"name":"off","type":"command","capability":"switch","label":"command: off()"},"type":"hubitatTrigger"},{"trigger":{"type":"attribute","properties":{"value":{"title":"SwitchState","type":"string"}},"additionalProperties":false,"required":["value"],"capability":"switch","attribute":"switch","label":"attribute: switch.off","value":"off","dataType":"ENUM"},"command":{"name":"setSwitchOff","label":"command: setSwitchOff()","type":"command"},"type":"smartTrigger"},{"trigger":{"type":"attribute","properties":{"value":{"title":"SwitchState","type":"string"}},"additionalProperties":false,"required":["value"],"capability":"switch","attribute":"switch","label":"attribute: switch.on","value":"on","dataType":"ENUM"},"command":{"name":"setSwitchOn","label":"command: setSwitchOn()","type":"command"},"type":"smartTrigger"},{"trigger":{"type":"attribute","properties":{"value":{"title":"HealthState","type":"string"}},"additionalProperties":false,"required":["value"],"capability":"healthCheck","attribute":"healthStatus","label":"attribute: healthStatus.*"},"command":{"name":"setHealthStatusValue","label":"command: setHealthStatusValue(healthStatus*)","type":"command","parameters":[{"name":"healthStatus*","type":"ENUM"}]},"type":"smartTrigger","mute":true}]}"""
+}
+
+import groovy.transform.CompileStatic
+import groovy.transform.Field
+
+@Field static final Integer iHttpSuccess=200
+@Field static final Integer iHttpError=400
+@Field static final String  sURI="https://api.smartthings.com"
+
+private String getAuthToken() {
+    return parent?.getAuthToken()
+}
+
+Map triggerScene(String sceneId) {
+    logDebug "${device.displayName} executing 'triggerScene($sceneId)'"
+    Map response = [statusCode:iHttpError]
+    
+    Map params = [
+        uri: sURI,
+        path: "/scenes/$sceneId/execute",
+        contentType: "application/json",
+        requestContentType: "application/json",
+        headers: [ Authorization: "Bearer ${getAuthToken()}" ]        
+    ]
+    try {
+        httpPost(params) { resp ->
+            logDebug "response data: ${resp.data}"
+            response.data = resp.data
+            response.statusCode = resp.status
+            logInfo "${device.displayName} executed SmartThings scene"
+        }
+    } catch (e) {
+        logWarn "${device.displayName} has triggerScene() error: $e"        
+    }
+    return response
+}
+
+private logInfo(msg)  { if(settings?.deviceInfoDisable != true) { log.info  "${msg}" } }
+private logDebug(msg) { if(settings?.deviceDebugEnable == true) { log.debug "${msg}" } }
+private logTrace(msg) { if(settings?.deviceTraceEnable == true) { log.trace "${msg}" } }
+private logWarn(msg)  { log.warn   "${msg}" }
+private logError(msg) { log.error  "${msg}" }
