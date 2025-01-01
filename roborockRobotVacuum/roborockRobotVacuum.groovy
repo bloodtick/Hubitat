51b19ac94c97148a5d048e5059e72685da109583
Bloodtick Jones <12273404+bloodtick@users.noreply.github.com>
2025-01-01 08:42:20 -0500

    1.1.11

--- roborockRobotVacuum/roborockRobotVacuum.groovy
+++ roborockRobotVacuum/roborockRobotVacuum.groovy
@@ -19,8 +19,8 @@
  *  Author: bloodtick
  *  Date: 2024-04-18
  */
-public static String version() {return "1.1.10"}
-@Field static final Boolean hubitatVersion239 = false
+public static String version() {return "1.1.11"}
+@Field static final Boolean hubitatVersion239 = true
 
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
@@ -550,7 +550,7 @@ void processMsg(Map message) {
                 logDebug "${device.displayName} command '$cmd.command' was accepted"
                 jsonValue?.result?.each{ result ->
                     if(cmd?.param==["get_status"]) {
-                        result.switch=(result?.in_cleaning?.toInteger()!=0 || result?.is_locating?.toInteger()!=0 || result?.is_exploring?.toInteger()!=0) ? "on" : "off"
+                        result.switch=(result?.in_cleaning?.toInteger()==1 || result?.is_locating?.toInteger()==1 || result?.is_exploring?.toInteger()==1) ? "on" : "off"
                         if(result?.battery?.toInteger()==100 && result?.state?.toInteger()==8) result.state=100
                         if(result?.clean_percent?.toInteger()==0 && result?.clean_area?.toInteger()>1) result.clean_percent=100
                         if(!stateDoNotRefreshCodes.contains(result.state)) { scheduleRefresh(60) } // some units don't send real time dps events
